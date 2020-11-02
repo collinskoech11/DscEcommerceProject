8f21c83ffbfd4505ff921855f1aba42ad4d36fca
BNanyingi <nanyingibarbara@gmail.com>
2020-11-02 12:02:13 +0300

    update

---
+++ GikombaOnline/gikomba-store/src/pages/productsPage/Data/WatchesData.js
@@ -0,0 +1,125 @@
+export default{
+    products:[
+        {
+            _id:'1',
+            name:'Watch',
+            category:'Mens Watch',
+            images:'/Images/Watches/images.jpg',
+            price:4500,
+            brand:'designer',
+            rating:4.5,
+            numReviews:10
+        },
+        {
+            _id:'2',
+            name:'Watch',
+            category:'Women Watch',
+            images:'/Images/Watches/download.jpg',
+            price:3000,
+            brand:'designer',
+            rating:4.5,
+            numReviews:10
+        },
+        {
+            _id:'3',
+            name:'Watch',
+            category:'mens watch',
+            images:'/Images/Watches/download (1).jpg',
+            price:5000,
+            brand:'designer',
+            rating:4.5,
+            numReviews:10
+        },
+        {
+            _id:'4',
+            name:'Watch',
+            category:'Womens Watch',
+            images:'/Images/Watches/download (2).jpg',
+            price:5000,
+            brand:'designer',
+            rating:4.5,
+            numReviews:10
+        },
+        {
+            _id:'5',
+            name:'Watch',
+            category:'Mens Watch',
+            images:'/Images/Watches/images (3).jpg',
+            price:6000,
+            brand:'designer',
+            rating:4.5,
+            numReviews:10
+        },
+        {
+            _id:'6',
+            name:'Watch',
+            category:'Mens Watch',
+            images:'/Images/Watches/images (4).jpg',
+            price:5000,
+            brand:'designer',
+            rating:4.5,
+            numReviews:10
+        },
+        {
+            _id:'7',
+            name:'Watch',
+            category:'Mens Watch',
+            images:'/Images/Watches/images (5).jpg',
+            price:6000,
+            brand:'designer',
+            rating:4.5,
+            numReviews:10
+        },
+        {
+            _id:'8',
+            name:'Watch',
+            category:'Mens Watch',
+            images:'/Images/Watches/images (6).jpg',
+            price:4000,
+            brand:'designer',
+            rating:4.5,
+            numReviews:10
+        },
+        {
+            _id:'9',
+            name:'Watch',
+            category:'Womens Watch',
+            images:'/Images/Watches/images (7).jpg',
+            price:5500,
+            brand:'designer',
+            rating:4.5,
+            numReviews:10
+        },
+        {
+            _id:'10',
+            name:'Watch',
+            category:'Mens Watch',
+            images:'/Images/Watches/images (8).jpg',
+            price:4000,
+            brand:'designer',
+            rating:4.5,
+            numReviews:10
+        },
+        {
+            _id:'11',
+            name:'Watch',
+            category:'MensWatch',
+            images:'/Images/Watches/images (9).jpg',
+            price:6000,
+            brand:'designer',
+            rating:4.5,
+            numReviews:10
+        },
+        {
+            _id:'12',
+            name:'Watch',
+            category:'Mens Watch',
+            images:'/Images/Watches/images (10).jpg',
+            price:6000,
+            brand:'designer',
+            rating:4.5,
+            numReviews:10
+        },
+    ]
+
+}
--- GikombaOnline/gikomba-store/src/pages/productsPage/Watches.js
+++ GikombaOnline/gikomba-store/src/pages/productsPage/Watches.js
@@ -1,13 +1,33 @@
-import React, { Component } from 'react'
+import React from 'react';
+import {Link} from 'react-router-dom';
+import WatchesData from './Data/WatchesData';
 
-export class Watches extends Component {
-    render() {
-        return (
-            <div>
-                <h1>This is the watches page</h1>
-            </div>
-        )
+function Watches() {
+    return (
+        <div className="content">
+            <ul className="products">
+                {
+                WatchesData.products.map(product => 
+                <li key={product._id}>
+                    <div className="product">
+                    <Link to={'/Product/' + product._id}>
+                        <img className="product-image" src ={product.images} alt="bag"/>
+                        </Link>
+                        <div className="product-name"><Link to={'/Product/' + product._id}>{product.name}</Link></div>
+                        <div className="product-brand">{product.brand}</div>
+                        <div className="product-price">Ksh.{product.price}</div>
+                        <div className="product-rating">4.5 stars ({product.numReviews})</div>
+                        </div>
+    
+                </li>    
+                )
+                    }
+                
+            </ul>
+        </div>
+                
+            )
+        
     }
-}
 
 export default Watches
