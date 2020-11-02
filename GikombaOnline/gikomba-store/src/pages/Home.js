274e9363b4cbb57512ee9c55234b85582fad0759
Collins Koech <collinskoechck34@gmail.com>
2020-11-02 10:22:23 +0300

    update

--- GikombaOnline/gikomba-store/src/pages/Home.js
+++ GikombaOnline/gikomba-store/src/pages/Home.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import WomensWear from './productsPage/WomensWear'
+import MensWear from './productsPage/MensWear'
 
 
 export default function Home() {
     return (
         <div>
         <WomensWear/>
+        <MensWear/>
         </div>
     )
 }
