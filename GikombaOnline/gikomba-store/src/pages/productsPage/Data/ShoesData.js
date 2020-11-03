d44f3e52238e23ddeaaeec4c7c41c69944040c00
BNanyingi <nanyingibarbara@gmail.com>
2020-11-03 09:44:36 +0300

    update

---
+++ GikombaOnline/gikomba-store/src/pages/productsPage/Data/ShoesData.js
@@ -0,0 +1,125 @@
+export default{
+    products:[
+        {
+            _id:'1',
+            name:'Shoes',
+            category:'Mens Shoes',
+            images:'/Images/Shoes/S1.jpg',
+            price:4500,
+            brand:'designer',
+            rating:4.5,
+            numReviews:10
+        },
+        {
+            _id:'2',
+            name:'Shoes',
+            category:'Mens Shoes',
+            images:'/Images/Shoes/S2.jpg',
+            price:3000,
+            brand:'designer',
+            rating:4.5,
+            numReviews:10
+        },
+        {
+            _id:'3',
+            name:'Shoes',
+            category:'Mens Shoes',
+            images:'/Images/Shoes/S3.jpg',
+            price:5000,
+            brand:'designer',
+            rating:4.5,
+            numReviews:10
+        },
+        {
+            _id:'4',
+            name:'Shoes',
+            category:'Womens Shoes',
+            images:'/Images/Shoes/S4.jpg',
+            price:5000,
+            brand:'designer',
+            rating:4.5,
+            numReviews:10
+        },
+        {
+            _id:'5',
+            name:'Shoes',
+            category:'Womens Shoes',
+            images:'/Images/Shoes/S5.jpg',
+            price:6000,
+            brand:'designer',
+            rating:4.5,
+            numReviews:10
+        },
+        {
+            _id:'6',
+            name:'Shoes',
+            category:'Mens Shoes',
+            images:'/Images/Shoes/S6.jpg',
+            price:5000,
+            brand:'designer',
+            rating:4.5,
+            numReviews:10
+        },
+        {
+            _id:'7',
+            name:'Shoes',
+            category:'Womens Shoes',
+            images:'/Images/Shoes/S7.jpg',
+            price:6000,
+            brand:'designer',
+            rating:4.5,
+            numReviews:10
+        },
+        {
+            _id:'8',
+            name:'Shoes',
+            category:'Mens Shoes',
+            images:'/Images/Shoes/S8.jpg',
+            price:4000,
+            brand:'designer',
+            rating:4.5,
+            numReviews:10
+        },
+        {
+            _id:'9',
+            name:'Shoes',
+            category:'Mens Shoes',
+            images:'/Images/Shoes/S9.jpg',
+            price:5500,
+            brand:'designer',
+            rating:4.5,
+            numReviews:10
+        },
+        {
+            _id:'10',
+            name:'Shoes',
+            category:'Mens Shoes',
+            images:'/Images/Shoes/S10.jpg',
+            price:4000,
+            brand:'designer',
+            rating:4.5,
+            numReviews:10
+        },
+        {
+            _id:'11',
+            name:'Shoes',
+            category:'Mens Shoes',
+            images:'/Images/Shoes/S11.jpg',
+            price:6000,
+            brand:'designer',
+            rating:4.5,
+            numReviews:10
+        },
+        {
+            _id:'12',
+            name:'Shoes',
+            category:'Womens Shoes',
+            images:'/Images/Shoes/S12.jpg',
+            price:6000,
+            brand:'designer',
+            rating:4.5,
+            numReviews:10
+        },
+    ]
+
+}
--- GikombaOnline/gikomba-store/src/pages/productsPage/Shoes.js
+++ GikombaOnline/gikomba-store/src/pages/productsPage/Shoes.js
@@ -1,13 +1,33 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import {Link} from 'react-router-dom';
+import ShoesData from './Data/ShoesData';
 
-export class Shoes extends Component {
-    render() {
+function Shoes() {
         return (
-            <div>
-                <h1>This is the shoes page</h1>
-            </div>
-        )
-    }
-}
+                 <div className="content">
+                        <ul className="products">
+                            {
+                             ShoesData.products.map(product => 
+                            <li key={product._id}>
+                                <div className="product">
+                                <Link to={'/Product/' + product._id}>
+                                    <img className="product-image" src ={product.images} alt="bag"/>
+                                    </Link>
+                                    <div className="product-name"><Link to={'/Product/' + product._id}>{product.name}</Link></div>
+                                    <div className="product-brand">{product.brand}</div>
+                                    <div className="product-price">Ksh.{product.price}</div>
+                                    <div className="product-rating">4.5 stars ({product.numReviews})</div>
+                                    </div>
+                
+                            </li>    
+                            )
+                                }
+                            
+                        </ul>
+                    </div>
+                            
+                        )
+                    
+                }
 
 export default Shoes
