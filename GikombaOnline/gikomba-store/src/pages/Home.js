c1fc9c9808532ae73911b974e30e892fea653666
Collins Koech <collinskoechck34@gmail.com>
2020-11-04 12:35:40 +0300

    added image carousel

--- GikombaOnline/gikomba-store/src/pages/Home.js
+++ GikombaOnline/gikomba-store/src/pages/Home.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import WomensWear from './productsPage/WomensWear'
+import CarouselPage from '../Navigators/CarouselPage'
 import MensWear from './productsPage/MensWear'
 
 
 export default function Home() {
     return (
         <div>
+        <CarouselPage/>
         <WomensWear/>
         <MensWear/>
         </div>
