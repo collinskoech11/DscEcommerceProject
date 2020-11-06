52c3117bb4d217bb4e94afbedfbe3af8f3b3f1be
BNanyingi <nanyingibarbara@gmail.com>
2020-11-06 13:04:08 +0300

    update

---
+++ GikombaOnline/backend/Data/WomensWearData.js
@@ -0,0 +1,125 @@
+export default{
+    products:[
+        {
+            _id:'1',
+            name:'Women wear',
+            category:'Women wear',
+            images:'/Images/WomensWear/w1.jpg',
+            price:6000,
+            brand:'designer',
+            rating:4.5,
+            numReviews:10
+        },
+        {
+            _id:'2',
+            name:'Women wear',
+            category:'Women wear',
+            images:'/Images/WomensWear/w2.jpg',
+            price:6000,
+            brand:'designer',
+            rating:4.5,
+            numReviews:10
+        },
+        {
+            _id:'3',
+            name:'Women wear',
+            category:'Women wear',
+            images:'/Images/WomensWear/w3.jpg',
+            price:6000,
+            brand:'designer',
+            rating:4.5,
+            numReviews:10
+        },
+        {
+            _id:'4',
+            name:'Women wear',
+            category:'Women wear',
+            images:'/Images/WomensWear/w4.jpg',
+            price:6000,
+            brand:'designer',
+            rating:4.5,
+            numReviews:10
+        },
+        {
+            _id:'5',
+            name:'Women wear',
+            category:'Women wear',
+            images:'/Images/WomensWear/w5.jpg',
+            price:6000,
+            brand:'designer',
+            rating:4.5,
+            numReviews:10
+        },
+        {
+            _id:'6',
+            name:'Women wear',
+            category:'Women wear',
+            images:'/Images/WomensWear/w6.jpg',
+            price:6000,
+            brand:'designer',
+            rating:4.5,
+            numReviews:10
+        },
+        {
+            _id:'7',
+            name:'Women wear',
+            category:'Women wear',
+            images:'/Images/WomensWear/w7.jpg',
+            price:6000,
+            brand:'designer',
+            rating:4.5,
+            numReviews:10
+        },
+        {
+            _id:'8',
+            name:'Women wear',
+            category:'Women wear',
+            images:'/Images/WomensWear/w8.jpg',
+            price:6000,
+            brand:'designer',
+            rating:4.5,
+            numReviews:10
+        },
+        {
+            _id:'9',
+            name:'Women wear',
+            category:'Women wear',
+            images:'/Images/WomensWear/w9.jpg',
+            price:6000,
+            brand:'designer',
+            rating:4.5,
+            numReviews:10
+        },
+        {
+            _id:'10',
+            name:'Women wear',
+            category:'Women wear',
+            images:'/Images/WomensWear/w10.jpg',
+            price:6000,
+            brand:'designer',
+            rating:4.5,
+            numReviews:10
+        },
+        {
+            _id:'11',
+            name:'Women wear',
+            category:'Women wear',
+            images:'/Images/WomensWear/w11.jpg',
+            price:6000,
+            brand:'designer',
+            rating:4.5,
+            numReviews:10
+        },
+        {
+            _id:'12',
+            name:'Women wear',
+            category:'Women wear',
+            images:'/Images/WomensWear/w12.jpg',
+            price:6000,
+            brand:'designer',
+            rating:4.5,
+            numReviews:10
+        },
+    ]
+
+}
