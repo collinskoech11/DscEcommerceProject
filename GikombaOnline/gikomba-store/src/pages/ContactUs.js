a5cf3c4548014d6e198c78e0006c1a407e3890c6
Collins Koech <collinskoechck34@gmail.com>
2020-11-18 10:08:46 +0300

    contact page update

--- GikombaOnline/gikomba-store/src/pages/ContactUs.js
+++ GikombaOnline/gikomba-store/src/pages/ContactUs.js
@@ -1,11 +1,114 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import { MDBRow, MDBCol, MDBCard, MDBCardBody, MDBIcon, MDBBtn, MDBInput } from "mdbreact";
 
 export class ContactUs extends Component {
     render() {
         return (
-            <div>
-                <h1>This is the Contact Page</h1>
-            </div>
+            <section className="my-5">
+      <h2 className="h1-responsive font-weight-bold text-center my-5">
+        Contact us
+      </h2>
+      <p className="text-center w-responsive mx-auto pb-5">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugit,
+        error amet numquam iure provident voluptate esse quasi, veritatis
+        totam voluptas nostrum quisquam eum porro a pariatur veniam.
+      </p>
+      <MDBRow>
+        <MDBCol lg="5" className="lg-0 mb-4">
+          <MDBCard>
+            <MDBCardBody>
+              <div className="form-header blue accent-1">
+                <h3 className="mt-2">
+                  <MDBIcon icon="envelope" /> Write to us:
+                </h3>
+              </div>
+              <p className="dark-grey-text">
+                We'll write rarely, but only the best content.
+              </p>
+              <div className="md-form">
+                <MDBInput
+                  icon="user"
+                  label="Your name"
+                  iconClass="grey-text"
+                  type="text"
+                  id="form-name"
+                />
+              </div>
+              <div className="md-form">
+                <MDBInput
+                  icon="envelope"
+                  label="Your email"
+                  iconClass="grey-text"
+                  type="text"
+                  id="form-email"
+                />
+              </div>
+              <div className="md-form">
+                <MDBInput
+                  icon="tag"
+                  label="Subject"
+                  iconClass="grey-text"
+                  type="text"
+                  id="form-subject"
+                />
+              </div>
+              <div className="md-form">
+                <MDBInput
+                  icon="pencil-alt"
+                  label="Icon Prefix"
+                  iconClass="grey-text"
+                  type="textarea"
+                  id="form-text"
+                />
+              </div>
+              <div className="text-center">
+                <MDBBtn color="light-blue">Submit</MDBBtn>
+              </div>
+            </MDBCardBody>
+          </MDBCard>
+        </MDBCol>
+        <MDBCol lg="7">
+          <div
+            id="map-container"
+            className="rounded z-depth-1-half map-container"
+            style={{ height: "400px" }}
+          >
+            <iframe
+              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3988.6338605984747!2d36.73452031470259!3d-1.3961496361495398!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x182f05cbfac861ff%3A0x680b30b9f4644fd1!2sMaasai%20Lodge%20Rongai!5e0!3m2!1sen!2ske!4v1605682258469!5m2!1sen!2ske"
+              title="This is a unique title"
+              width="100%"
+              height="100%"
+              frameBorder="0"
+              style={{ border: 0 }}
+            />
+          </div>
+          <br />
+          <MDBRow className="text-center">
+            <MDBCol md="4">
+              <MDBBtn tag="a" floating color="blue" className="accent-1">
+                <MDBIcon icon="map-marker-alt" />
+              </MDBBtn>
+              <p>Nairobi</p>
+              <p className="mb-md-0">Kenya</p>
+            </MDBCol>
+            <MDBCol md="4">
+              <MDBBtn tag="a" floating color="blue" className="accent-1">
+                <MDBIcon icon="phone" />
+              </MDBBtn>
+              <p>0101170937</p>
+              <p className="mb-md-0">Mon - Fri, 8:00-22:00</p>
+            </MDBCol>
+            <MDBCol md="4">
+              <MDBBtn tag="a" floating color="blue" className="accent-1">
+                <MDBIcon icon="envelope" />
+              </MDBBtn>
+              <p>info@gikosh.com</p>
+              <p className="mb-md-0">sale@gikosh.com</p>
+            </MDBCol>
+          </MDBRow>
+        </MDBCol>
+      </MDBRow>
+    </section>
         )
     }
 }
