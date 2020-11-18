aade443e123bf52376f87165af9bab29578963b0
Collins Koech <collinskoechck34@gmail.com>
2020-11-18 09:44:54 +0300

    added live location API

--- GikombaOnline/gikomba-store/src/pages/Location.js
+++ GikombaOnline/gikomba-store/src/pages/Location.js
@@ -3,9 +3,17 @@ import React, { Component } from 'react'
 export class Location extends Component {
     render() {
         return (
-            <div>
-                <h1>This is the Location Page</h1>
-            </div>
+            <main class=" m-0 p-0">
+  <div class="container-fluid m-0 p-0">
+
+    <!--Google map-->
+    <div id="map-container-google-4" class="z-depth-1-half map-container-4" style="height: 500px">
+      <iframe src="https://maps.google.com/maps?q=manhatan&t=&z=13&ie=UTF8&iwloc=&output=embed" frameborder="0"
+        style="border:0" allowfullscreen></iframe>
+    </div>
+
+  </div>
+</main>
         )
     }
 }
