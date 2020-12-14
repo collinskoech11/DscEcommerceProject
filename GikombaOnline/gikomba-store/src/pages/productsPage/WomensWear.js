9db33f5129a92591f04f9a9eaea01a9b0f234388
Diana-nyamai <dnyamai.dn@gmail.com>
2020-12-14 13:36:57 +0300

    update pagination

--- GikombaOnline/gikomba-store/src/pages/productsPage/WomensWear.js
+++ GikombaOnline/gikomba-store/src/pages/productsPage/WomensWear.js
@@ -1,5 +1,6 @@
 import React from 'react'; 
 import {Link} from 'react-router-dom';
+import Pages from '../Pages';
 import WomensWearData from './Data/WomensWearData';
 import StarRating from './StarRating';
 
