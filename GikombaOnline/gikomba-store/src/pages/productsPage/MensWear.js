2f61c909cbb2a05f872875ead121e36d7e6963a0
Diana-nyamai <dnyamai.dn@gmail.com>
2020-12-14 13:31:09 +0300

    update

--- GikombaOnline/gikomba-store/src/pages/productsPage/MensWear.js
+++ GikombaOnline/gikomba-store/src/pages/productsPage/MensWear.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import {Link} from 'react-router-dom';
+import Pages from '../Pages';
 import MensWearData from './Data/MensWearData';
 import StarRating from './StarRating';
 
@@ -26,6 +27,7 @@ export class MensWear extends Component {
                     }
                 
             </ul>
+            <Pages/>
         </div>
         )
     }
