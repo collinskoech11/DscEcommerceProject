0de1d69d55343909c84c300928f355b9be61794a
BNanyingi <nanyingibarbara@gmail.com>
2020-12-15 11:13:38 +0300

    update

--- GikombaOnline/gikomba-store/src/App.js
+++ GikombaOnline/gikomba-store/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Switch, Route } from "react-router-dom";
+import { Switch } from "react-router-dom";
 import './App.css';
 import './css/products/WomensWear.css';
 import './css/Screens/ProductScreen.css';
