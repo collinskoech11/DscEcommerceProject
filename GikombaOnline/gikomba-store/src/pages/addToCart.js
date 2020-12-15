8853ca615a277c5d8a66b09d0b3cc5f0eabe0007
Collins Koech <collinskoechck34@gmail.com>
2020-12-15 10:05:39 +0300

    added cart item

--- GikombaOnline/gikomba-store/src/pages/addToCart.js
+++ GikombaOnline/gikomba-store/src/pages/addToCart.js
@@ -1,9 +1,21 @@
-import React, { Component } from 'react'
+import React, { Component, useContext } from 'react';
+import { CartContext } from '../../contexts/CartContext';
+
+import CartItem from './CartItem';
+import styles from './CartProducts.module.scss';
 
 export class addToCart extends Component {
     render() {
         return (
-          <h1>This is the cart page</h1>
+          <div className={styles.p__container}>
+            <div className="card card-body border-0">
+
+                {
+                    cartItems.map(product =>  <CartItem key={product.id} product={product}/>)
+                }
+
+            </div>
+        </div>
         )
       }
     }
