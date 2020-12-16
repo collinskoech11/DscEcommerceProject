a2a8802180cc1201765efba1c1ece36a7b6d6c67
Diana-nyamai <dnyamai.dn@gmail.com>
2020-12-16 15:26:38 +0300

    about page update

--- GikombaOnline/gikomba-store/src/pages/AboutPage.js
+++ GikombaOnline/gikomba-store/src/pages/AboutPage.js
@@ -1,13 +1,12 @@
-import React, { Component } from 'react'
+import React from 'react'
 
-export class AboutPage extends Component {
-    render() {
-        return (
-            <div>
-               <h1>this is the about page</h1>
-            </div>
-        )
-    }
+function AboutPage() {
+    return (
+        <div>
+        Gikosh.com is a platform where you will be able to sell your thrift items online.
+         Gikosh will ensure you reach out as many clients as possible locally and also globally
+        </div>
+    )
 }
 
 export default AboutPage
