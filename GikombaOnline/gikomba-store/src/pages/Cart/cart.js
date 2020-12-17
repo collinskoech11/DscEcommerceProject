deac6e87b451f829d65a1c197867a3f5f1cd722d
Collins Koech <collinskoechck34@gmail.com>
2020-12-17 21:44:33 +0300

    update cart.js

---
+++ GikombaOnline/gikomba-store/src/pages/Cart/cart.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+
+export class Login extends Component {
+    render() {
+        return (
+        <h1>This is the cart page</h1>
+        )}
+  }
+
+export default Login
