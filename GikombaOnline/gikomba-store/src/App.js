7601144ae3d19aca6e3cbdc6976fcd3c2f87e349
Collins Koech <collinskoechck34@gmail.com>
2020-12-20 01:41:20 +0300

    update

--- GikombaOnline/gikomba-store/src/App.js
+++ GikombaOnline/gikomba-store/src/App.js
@@ -17,6 +17,7 @@ import WomensWear from './pages/productsPage/WomensWear';
 import WomensProductScreen from './pages/productsPage/productscreens/WomensProductScreen';
 import SignUp from './pages/SignUp';
 import CheckOut from './pages/Checkout/CheckOut';
+import cart from './pages/Cart/cart';
 
 
 function App() {
@@ -28,6 +29,7 @@ function App() {
      <Route path="/AboutPage" component={AboutPage}/>
      <Route path="/ContactUs" component={ContactUs}/>
      <Route path="/Location" component={Location}/>
+     <Route path="/cart" component={cart}/>
      <Route path="/checkOut" component={CheckOut}/>
      
      
