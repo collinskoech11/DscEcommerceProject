702cffe065533a6c40d35af64d049c081014d39a
Diana-nyamai <dnyamai.dn@gmail.com>
2020-12-22 13:29:52 +0300

    update

--- GikombaOnline/gikomba-store/src/pages/Cart/cart.js
+++ GikombaOnline/gikomba-store/src/pages/Cart/cart.js
@@ -1,71 +1,13 @@
-import React from 'react'
-import {Link} from 'react-router-dom'
-import {MDBIcon} from 'mdbreact'
-import WomensWearData from './Data/WomensWearData';
-import StarRating from '../StarRating';
+import React, { Component } from 'react'
 
-
-function cart(props) {
-      console.log(props.match.params.id);
-      const product = WomensWearData.products.find(x => x._id === props.match.params.id);
-       return(
-       <div>
-       <div className="back-to-result"> 
-               <Link to="/">Back to result</Link>
-           </div>
-            (
-                <div className="details">
-               <div className="details-image">
-                   <img src={product.images} alt="product"/>
-               </div>
-               <div className="details-info">
-                 <ul>
-                     <li>
-                         <h4>{product.name}</h4>
-                     </li>
-                     <li>
-                         <StarRating/> ({product.numReviews} Reviews)
-                     </li>
-                     <li>
-                       Price:<b>Ksh.{product.price}</b>
-                     </li>
-                     <li>
-                         Description:
-                         <div>
-                             {product.description}
-                         </div>
-                     </li>
-                 </ul>
-               </div>
-           <div className="details-action">
-               <ul>
-                   <li>
-                       price: {product.price}
-                   </li>
-                   <li>
-                       status: {product.status}
-                   </li>
-                   <li>
-                       Qty:<select>
-                           <option>1</option>
-                           <option>2</option>
-                           <option>3</option>
-                           <option>4</option>
-                           <option>5</option>
-                       </select>
-                   </li>
-                   <li>
-                       <button className="button" onclick={() => {window.location.href="/cart"}}>
-                       Add to cart <MDBIcon icon="cart-plus"/></button>
-                   </li>
-               </ul>
-           </div>
-           </div>
-        )
+export class cart extends Component {
+  render() {
+    return (
+      <div>
         
-</div>
-           
- )   
+      </div>
+    )
+  }
 }
 
 export default cart
